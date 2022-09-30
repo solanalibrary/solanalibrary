ed511c54a215107fb1a2512eefc90a9677ec02ba
Ennio Nasca <web3librarian@gmail.com>
2022-09-30 14:09:04 +0200

    feat: track current ama
    feat: track current ama
    
    feat: track current ama
    
    feat: track current ama
    
    feat: track current ama

--- script.js
+++ script.js
@@ -7,21 +7,17 @@ $.getJSON("https://raw.githubusercontent.com/solanalibrary/solanalibrary/main/pl
           timeupdate: function(){
               const active_index = Amplitude.getActiveIndex();
               const duration = Amplitude.getSongPlayedSeconds()
-              console.log(active_index, duration);
+              if (duration > 30) {
+                setCookie("ama", active_index, 7);
+                setCookie("seconds", Math.floor(duration), 7);
+              }
           }
         }
     });
     // https://stackoverflow.com/questions/51081488/continue-song-on-the-next-page-with-amplitude-js
-
     var playlist = document.getElementsByClassName("white-player-playlist")[0];
 
     for(var i = items.length - 1; i >= 0 ; i--) {
-        //var h5 = document.createElement("h5");
-        //h5.innerHTML = items[i].title;
-        //news.appendChild(h5);
-        //var p = document.createElement("p");
-        //p.innerHTML = items[i].author;
-        //news.appendChild(p);
         var container = document.createElement("div");
         
         container.className = "white-player-playlist-song amplitude-song-container amplitude-play-pause";
@@ -38,22 +34,10 @@ $.getJSON("https://raw.githubusercontent.com/solanalibrary/solanalibrary/main/pl
 
     }
 
-    Amplitude.skipTo(0, items.length-1);
-
+    recoverAMA(items.length - 1);
     Amplitude.bindNewElements();
 
 });
-
-/*
-<div class="white-player-playlist-song amplitude-song-container amplitude-play-pause" data-amplitude-song-index="0">
-  <img src="https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"/>
-
-  <div class="playlist-song-meta">
-    <span class="playlist-song-name">Risin' High (feat Raashan Ahmad)</span>
-    <span class="playlist-artist-album">Ancient Astronauts &bull; We Are to Answer</span>
-  </div>
-</div>
-*/
 
 
 /*
@@ -162,4 +146,40 @@ function appendToSongDisplay( song, index ){
     Appends the song element to the playlist
   */
   playlistElement.appendChild( playlistSong );
+}
+
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+
+function recoverAMA(default_ama) {
+  const ama_id = getCookie("ama");
+  if (ama_id != "" & ama_id != null) {
+    const seconds = getCookie("seconds");
+    console.log(`You were listening to ${ama_id} at ${seconds}`);
+    Amplitude.skipTo(seconds, ama_id);
+  } else {
+    setCookie("ama", default_ama, 7);
+    setCookie("seconds", 0, 7);
+    Amplitude.skipTo(0, default_ama);
+  }
 }
